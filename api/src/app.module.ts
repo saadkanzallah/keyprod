357067936fa5d8782bab01a752a49029c859e262
saadkanzallah <32311311+saadkanzallah@users.noreply.github.com>
2023-06-27 00:28:41 +0200

    feat: install & config swagger

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,13 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { PrismaService } from './prisma/prisma.service';
-import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  imports: [],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('KeyProd')
+    .setDescription('The KeyProd API')
+    .setVersion('1.0')
+    .build();
+
+  const catDocument = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, catDocument);
+
   await app.listen(3000);
 }
 bootstrap();
