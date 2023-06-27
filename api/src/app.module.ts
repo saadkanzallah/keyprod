701aa7f81eddc474c99de69e077a8a4414595b25
saadkanzallah <32311311+saadkanzallah@users.noreply.github.com>
2023-06-27 15:57:31 +0200

    feat: init CRUD ( products, orders, parcels )

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,9 +3,19 @@ import { AuthModule } from './auth/auth.module';
 import { AccessTokenStrategy } from './auth/strategy/accessToken.strategy';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
+import { ParcelsModule } from './parcels/parcels.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal: true}), AuthModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}), 
+    AuthModule, 
+    PrismaModule, 
+    ProductsModule, 
+    OrdersModule, 
+    ParcelsModule
+  ],
   controllers: [],
   providers: [AccessTokenStrategy],
 })
