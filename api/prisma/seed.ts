645ac0c11bf264b7a9edf60d5b7abd2f3f5468f5
saadkanzallah <32311311+saadkanzallah@users.noreply.github.com>
2023-06-27 20:00:17 +0200

    feat: seed Order table

--- api/prisma/seed.ts
+++ api/prisma/seed.ts
@@ -4,6 +4,7 @@ import { faker } from '@faker-js/faker';
 const prisma  = new PrismaClient();
 const roles = ['ADMIN', 'AGENT', 'USER'];
 const productCount = 10;
+const OrderCount = 5;
 const fakerUser = (role: string): any => ({
     email: role.toLowerCase() + '@keyprod.fr',
     password: 'XXXXXX', // TODO : hash password
@@ -12,18 +13,33 @@ const fakerUser = (role: string): any => ({
     role
 });
 
-const fakerProduct = (): any => ({
+const fakerProduct = (orderId: string = null): any => ({
     category: faker.helpers.arrayElement(['KEYNETIC', 'KEYVIBE']),
-    version: 'V' + faker.number.int({ min: 1, max: 9 }) ,
+    version: 'V' + faker.number.int({ min: 1, max: 9 }),
     code: faker.string.alphanumeric({casing: 'upper', length: 6}),
     weight: faker.number.int({ min: 1, max: 100 }),
+    orderId
 });
+
+const fakerOrder = (): any => ({
+    client: faker.person.fullName() ,
+    code: faker.string.alphanumeric({casing: 'upper', length: 6}),
+});
+
+const fakerProductsOrder = async (total: number, orderId: string): Promise<any> => {
+    for (let i = 1; i <= total; i++) {
+        console.log(`Seeding Product ${i} / Order ${orderId}...`);
+        await prisma.product.create({ data: fakerProduct(orderId) });
+    }
+}
     
 async function main() {
     console.log(`Clearing User table...`);
     await prisma.user.deleteMany();
     console.log(`Clearing Product table...`);
     await prisma.product.deleteMany();
+    console.log(`Clearing Product table...`);
+    await prisma.order.deleteMany();
     console.log(`Seeding Start...`);
     /// --------- Users ---------------
     console.log(`Seeding Users...`);
@@ -39,8 +55,16 @@ async function main() {
         await prisma.product.create({ data: fakerProduct() });
     }
 
-    console.log(`Seeding End...`);
+    /// --------- Orders ---------------
+    console.log(`Seeding ${OrderCount} Orders...`);
+    for (let i = 1; i <= OrderCount; i++) {
+        console.log(`Seeding Order ${i}...`);
+        const newOrder = await prisma.order.create({ data: fakerOrder() });
+        const totalOrder = faker.number.int({ min: 1, max: 9 });
+        fakerProductsOrder(totalOrder, newOrder.id);
+    }
 
+    console.log(`Seeding End...`);
 };
     
       
